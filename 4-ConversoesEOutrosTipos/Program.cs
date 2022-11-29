dec40a22ee89206a820befdf1268ec3fef1494e5
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-11-29 17:10:37 -0300

    Adicionar arquivos de projeto.

---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario = 3000.15;
+        Console.WriteLine(salario);
+
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+
+        long x = 20000000000000000;
+        Console.WriteLine(x);
+
+        short y = 15000;
+        Console.WriteLine(y);
+
+        float altura = 1.62f;
+        Console.WriteLine(altura);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
